93ed4ab08d54e181d9bcc3d5641f1ae28e0b40c2
teclu <saintpatricktay@gmail.com>
2021-06-02 17:41:01 +0800

    fix: remove listeners on component unmount

--- src/pages/board/components/BoardMessages.tsx
+++ src/pages/board/components/BoardMessages.tsx
@@ -72,17 +72,29 @@ const BoardMessages = ({ boardMessages }: BoardMessagesProps): JSX.Element => {
   /*
    * Listen for changes in window width.
    */
-  React.useEffect((): void => {
-    window
-      .matchMedia(getMinWidthQuery(BreakpointWidth.LG))
-      .addEventListener('change', (event: MediaQueryListEvent): void =>
-        setIsLgWidth(event.matches),
-      );
-    window
-      .matchMedia(getMinWidthQuery(BreakpointWidth.XL))
-      .addEventListener('change', (event: MediaQueryListEvent): void =>
-        setIsXlWidth(event.matches),
-      );
+  React.useEffect((): (() => void) => {
+    const lgCallback = (event: MediaQueryListEvent): void =>
+      setIsLgWidth(event.matches);
+
+    const xlCallback = (event: MediaQueryListEvent): void =>
+      setIsXlWidth(event.matches);
+
+    const lgMediaQueryList: MediaQueryList = window.matchMedia(
+      getMinWidthQuery(BreakpointWidth.LG),
+    );
+
+    lgMediaQueryList.addEventListener('change', lgCallback);
+
+    const xlMediaQueryList: MediaQueryList = window.matchMedia(
+      getMinWidthQuery(BreakpointWidth.XL),
+    );
+    xlMediaQueryList.addEventListener('change', xlCallback);
+
+    // Remove the listeners when component is unmounted.
+    return (): void => {
+      lgMediaQueryList.removeEventListener('change', lgCallback);
+      xlMediaQueryList.removeEventListener('change', xlCallback);
+    };
   }, []);
 
   return (
